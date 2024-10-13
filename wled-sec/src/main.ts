546371bb88ac28fc4e80c85d613f4d864d36a6a7
Christian Schwinne <dev.aircoookie@gmail.com>
2024-10-13 22:45:43 +0200

    HMAC request message logic

--- wled-sec/src/main.ts
+++ wled-sec/src/main.ts
@@ -9,4 +9,16 @@ hashButton.addEventListener('click', () => hashPassword())
 let hmacButton = document.querySelector<HTMLButtonElement>('#hmacButton')!
 hmacButton.addEventListener('click', () => signMessage())
 
+let hostInput = document.querySelector<HTMLInputElement>('#hostInput')!
+let connectButton = document.querySelector<HTMLButtonElement>('#connectButton')!
+connectButton.addEventListener('click', () => {
+  let host = 'http://' + hostInput.value
+  let uiWindow = window.open(host, 'wled-ui')
+  if (uiWindow) {
+    //uiWindow.postMessage('{"wled-rc":"opened"}', host)
+  } else {
+    document.getElementById('connectionInfo')!.textContent = 'Failed to open window';
+  }
+})
+
 onmessage = (event) => { handleMessageEvent(event) };
--- wled-sec/src/messages.ts
+++ wled-sec/src/messages.ts
@@ -3,9 +3,21 @@ import { signMessage } from "./crypto";
 export function handleMessageEvent(event : MessageEvent) {
   console.log(`Received message: ${event.data}`);
   console.log(`origin: ${event.origin}`);
-  console.log(`source: ${event.source}`);
+  //console.log(`source: ${event.source}`);
   // TODO: Check if the message is from the expected source
-  signMessage(event.data).then((signature) => {
-    event.source!.postMessage(signature, {'targetOrigin':event.origin});
-  });
+  try {
+    var json = JSON.parse(event.data)
+  } catch (e) {
+    console.log(`Error parsing JSON: ${e}`);
+    return;
+  }
+  if (json['wled-ui'] === 'onload') {
+    event.source!.postMessage('{"wled-rc":"ready"}', {'targetOrigin':event.origin});
+    //return;
+  } else if (json['wled-ui'] === 'hmac-req') {
+    signMessage(JSON.stringify(json['msg'])).then((signature) => {
+      var msgObject = {'wled-rc':'hmac', 'msg': json['msg'], 'sig': signature};
+      event.source!.postMessage(JSON.stringify(msgObject), {'targetOrigin':event.origin});
+    });
+  }
 }
